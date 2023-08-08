5849ceb01d7095f937a03fd165eebe4c0cc49c91
Ivan Alamos <ivan_alamos@outlook.com>
2023-08-07 20:10:26 -0600

    Fix referer

--- src/mx/ipn/upiicsa/segsw/el_rincon_del_tragon/servlet/LogoutServlet.java
+++ src/mx/ipn/upiicsa/segsw/el_rincon_del_tragon/servlet/LogoutServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import mx.ipn.upiicsa.segsw.el_rincon_del_tragon.util.Utility;
 
 /**
  * 
